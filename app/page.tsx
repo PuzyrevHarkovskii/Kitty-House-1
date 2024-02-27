62628491d7660e9d358d4b38bfb9964d7f801782
Владислав <pahnetstarushkami@bk.ru>
2024-02-27 14:42:18 +0300

    New features

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@ import HeroSection from "@/components/HeroSection";
 import Features from "@/components/Features";
 import Testimonials from "@/components/Testimonials";
 import ContactForm from "@/components/ContactForm";
+import ThreeTierPricingHorizontal from "@/components/Pricelist"; // Pricelist
+import Carousel from "@/components/Carousel";
 
 export default function Home() {
   return (
@@ -10,6 +12,8 @@ export default function Home() {
     <HeroSection/>
     <Features/>
     <Testimonials/>
+    <Carousel/>
+    <ThreeTierPricingHorizontal/>
     <ContactForm/>
     </>
   );
