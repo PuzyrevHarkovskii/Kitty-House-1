fbefe6bec4f5b2260bf2099a687d61547f8366bb
Владислав <pahnetstarushkami@bk.ru>
2024-02-27 13:17:21 +0300

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+
+import HeroSection from "@/components/HeroSection";
+import Features from "@/components/Features";
+import Testimonials from "@/components/Testimonials";
+import ContactForm from "@/components/ContactForm";
+
+export default function Home() {
+  return (
+    <>
+    <HeroSection/>
+    <Features/>
+    <Testimonials/>
+    <ContactForm/>
+    </>
+  );
+}
